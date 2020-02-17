9ee3333b0a479d60339e3afce3cf94fb8c702d3b
Nicole <nicole.tomlinson92@gmail.com>
2020-02-17 22:38:23 +0000

    reformatted the table and added delete icons

--- my-app/src/list.js
+++ my-app/src/list.js
@@ -3,31 +3,40 @@ import React from 'react';
 class List extends React.Component {
   render() {
     return (
-      <table class="table table-hover table-dark">
-        <thead>
-          <tr class ="table-warning">
-            <th scope="col">Bill Type</th>
-            <th scope="col">Renewal Date</th>
-            <th scope="col">Name of Bill Provider</th>
-            <th scope="col">Delete</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">INPUT DATA</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>Delete button here</td>
 
-          </tr>
-          <tr>
-            <th scope="row">INPUT DATA</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>Delete button here</td>
-          </tr>
-        </tbody>
-      </table>
+      <div class="table-responsive">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Bill Type</th>
+              <th scope="col">Renewal Date</th>
+              <th scope="col">Name of Bill Provider</th>
+              <th scope="col">Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Cell</td>
+              <td>Cell</td>
+              <td><button class="btn"><i class="fa fa-trash"></i></button></td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Cell</td>
+              <td>Cell</td>
+              <td><button class="btn"><i class="fa fa-trash"></i></button></td>
+
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Cell</td>
+              <td>Cell</td>
+              <td><button class="btn"><i class="fa fa-trash"></i></button></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
